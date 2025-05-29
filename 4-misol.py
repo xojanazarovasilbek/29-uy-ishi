bcd6b73bdec2275121e26e617c3dd8a6a7f84677
xojanazarovasilbek <xojanazarovasilbek1908@gmail.com>
2025-05-29 09:31:12 +0500

    4dan 10-misolgacha shu yerda

---
+++ 4-misol.py
@@ -0,0 +1,31 @@
+# 4-masala
+# def say_hello(name, city, state):
+#     full_name = ' '.join(name) 
+#     return f"Hello, {full_name}! Welcome to {city}, {state}!"
+# print(say_hello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+
+# 6-masala
+# def sort_textbooks(lst):
+#     return sorted(lst)
+# books = ['Algebra', 'history', 'Geometry', 'english']
+# print(sort_textbooks(books))
+
+# 7-masala
+# def square_sum(numbers):
+#     return sum(x**2 for x in numbers)
+# print(square_sum([1, 2, 2]))
+
+# 8-masala
+# def invert(lst):
+#     return [-x for x in lst]
+# print(invert([1, -2, 3, -4])) 
+
+# 9-masala
+# def reverse_list(lst):
+#     return lst[::-1]
+# print(reverse_list([10, 20, 30]))
+
+# 10-masala
+def fix_the_meerkat(arr):
+    return arr[::-1]
+print(fix_the_meerkat(['tail', 'body', 'head'])) 
